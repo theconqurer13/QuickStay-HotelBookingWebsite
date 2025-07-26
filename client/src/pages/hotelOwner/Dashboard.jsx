816e3abfb363f0b245963b02b4c13d9541e318cb
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-07-26 11:15:49 +0530

    ...

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -3,7 +3,11 @@ import Title from '../../components/Title'
 import { assets, dashboardDummyData } from '../../assets/assets'
 
 const Dashboard = () => {
-    const [data, setData] = useState(dashboardDummyData);
+    const [data, setData] = useState({
+        bookings:[],
+        totalBookings:0,
+        totalRevenue:0
+    });
   return (
     <div>
         <Title align='left' font="outfit" title = 'Dashboard' subTitle='Monitor your room listings, track bookigs and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations.'/>
--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -22,6 +22,22 @@ const ListRoom = () => {
         }
     }
 
+    // Toggle availability of the room
+    const toggleAvailability = async (roomId)=>{
+        const {data} = await axios.post('/api/rooms/toggle-availability',{roomId},
+            {headers:{Authorization: `Bearer ${await getToken()}`}});
+            if(data.success){
+                toast.success(data.message)
+                fetchRooms()
+            }else{
+                toast.error(data.message)
+            }
+        
+    }
+
+
+
+
     useEffect(()=>{
         if(user){
             fetchRooms()
@@ -57,7 +73,7 @@ const ListRoom = () => {
                                  <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
                                     <div class="flex flex-wrap items-center justify-center gap-12">
                                         <label class="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                                            <input type="checkbox" class="sr-only peer" checked />
+                                        <input onChange={()=> toggleAvailability(item._id)} type="checkbox" class="sr-only peer" checked={item.isAvailable} />
                                             <div class="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-indigo-600 transition-colors duration-200"></div>
                                             <span class="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>                                  
                                         </label>
